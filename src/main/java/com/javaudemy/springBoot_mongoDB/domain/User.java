188776cc8e3598f225f37dfdc95c79d284f57e50
Felipe Milana <felipe.milana97@gmail.com>
2021-01-13 16:22:43 -0300

    Connecting to MongoDB and creating User Service and Repository

--- src/main/java/com/javaudemy/springBoot_mongoDB/domain/User.java
+++ src/main/java/com/javaudemy/springBoot_mongoDB/domain/User.java
@@ -2,12 +2,17 @@ package com.javaudemy.springBoot_mongoDB.domain;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
 public class User implements Serializable {
 
 	
 	private static final long serialVersionUID = 1L;
 	
 	//attributes
+	@Id
 	private String id;
 	private String name;
 	private String email;
