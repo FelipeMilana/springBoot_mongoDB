5dc2dd58aa043084ffc7804c13d6e0899c06eecd
Felipe Milana <felipe.milana97@gmail.com>
2021-01-14 16:20:04 -0300

    User insert, delete and update

--- src/main/java/com/javaudemy/springBoot_mongoDB/resources/UserResource.java
+++ src/main/java/com/javaudemy/springBoot_mongoDB/resources/UserResource.java
@@ -1,14 +1,20 @@
 package com.javaudemy.springBoot_mongoDB.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.javaudemy.springBoot_mongoDB.domain.User;
 import com.javaudemy.springBoot_mongoDB.dto.UserDTO;
@@ -34,4 +40,26 @@ public class UserResource {
 		UserDTO userDto = new UserDTO(service.findById(id));
 		return ResponseEntity.ok().body(userDto);
 	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> delete(@PathVariable String id, @RequestBody UserDTO userDto){
+		User obj = service.fromDTO(userDto);
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
--- src/main/java/com/javaudemy/springBoot_mongoDB/services/UserService.java
+++ src/main/java/com/javaudemy/springBoot_mongoDB/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.javaudemy.springBoot_mongoDB.domain.User;
+import com.javaudemy.springBoot_mongoDB.dto.UserDTO;
 import com.javaudemy.springBoot_mongoDB.repositories.UserRepository;
 import com.javaudemy.springBoot_mongoDB.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,34 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> opt = repository.findById(id);
 		return opt.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public void delete(String id) {
+		//verification if this id exist or not
+		findById(id);
+		repository.deleteById(id);
+	}
+	
+	public User update(String id, User obj) {
+		//return original User from database with this id
+		User newObj = findById(id); 
+		//update other fields from obj to newObj
+		updateData(newObj, obj);
+		//save newObj updated
+		return repository.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+	
+	//can be in UserDTO
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
