ea581e0ba3f04516f9e2222abd506791e8d81406
Felipe Milana <felipe.milana97@gmail.com>
2021-01-19 16:43:31 -0300

    Entity Post and Post Repository

--- src/main/java/com/javaudemy/springBoot_mongoDB/config/Instantiation.java
+++ src/main/java/com/javaudemy/springBoot_mongoDB/config/Instantiation.java
@@ -1,12 +1,15 @@
 package com.javaudemy.springBoot_mongoDB.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.javaudemy.springBoot_mongoDB.domain.Post;
 import com.javaudemy.springBoot_mongoDB.domain.User;
+import com.javaudemy.springBoot_mongoDB.repositories.PostRepository;
 import com.javaudemy.springBoot_mongoDB.repositories.UserRepository;
 
 @Configuration
@@ -14,16 +17,23 @@ public class Instantiation implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, Instant.parse("2018-08-21T00:00:00Z"), "Partiu viagem", "Vou viajar para São Paulo, abraços!", maria);
+		Post post2 = new Post(null, Instant.parse("2018-08-23T00:00:00Z"), "Bom dia!", "Acordei feliz hoje", maria);
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
-
 }
